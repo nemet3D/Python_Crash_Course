4fca7c295e35c855aaf182fe2858114a9bf79dad
nemet3D <nemet3d@gmail.com>
2018-12-27 11:35:03 +0200

    Chapter_2

---
+++ Chapter 2 - Variables and Simple Data Types/favorite_number.py
@@ -0,0 +1,6 @@
+# Store your favorite number in a variable . Then, using
+# that variable, create a message that reveals your favorite number . Print that
+# message.
+
+favorite_number = 8
+print('My favorite number is ' + str(favorite_number) + ".")
---
+++ Chapter 2 - Variables and Simple Data Types/numer_eight.py
@@ -0,0 +1,8 @@
+# Number Eight: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8 . Be sure to enclose your operations
+# in print statements to see the results.
+
+print(5 + 3)
+print(12 - 4)
+print(2 * 4)
+print(int(24 / 3))
